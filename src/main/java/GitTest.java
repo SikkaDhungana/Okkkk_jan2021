1d7c877cd535e0a4c3c9023a408245c38a942b3a
SikkaDhungana <sikkadhungana@gmail.com>
2021-05-31 21:24:20 -0400

    second commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,13 @@
+
+public class GitTest {
+ public static void main(String[] args) {
+	 String a = "ball";
+	 int b = 12;
+	 System.out.println(b+a+b);
+	 printMe();
+ }
+ 
+public static void printMe() {
+	System.out.println("git test");
+}
+}
